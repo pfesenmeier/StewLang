50c0dc54bd4b34de3c67d4b28c199fb3e459eb7b
Paul Fesenmeier <pfesenmeier@gmail.com>
2024-10-15 21:10:16 -0400

    new scanner

--- scanner.ts
+++ scanner.ts
@@ -12,6 +12,33 @@ export class Scanner {
             while (!this.cannotAdvance()) {
                 const current = this.advance();
                 switch (current) {
+                    case "-":
+                        yield this.createToken(
+                            TokenType.DASH,
+                            current,
+                        );
+                        break;
+                    case ",":
+                        yield this.createToken(
+                            TokenType.COMMA,
+                            current,
+                        );
+                        break;
+                    case "\r":
+                        if (!this.cannotAdvance() && this.peek() === "\n") {
+                            const next = this.advance();
+                            yield this.createToken(
+                                TokenType.NEWLINE,
+                                current + next,
+                            );
+                        }
+                        break;
+                    case "\n":
+                        yield this.createToken(
+                            TokenType.NEWLINE,
+                            current,
+                        );
+                        break;
                     case "{":
                         yield this.createToken(
                             TokenType.LEFT_CURLY_BRACE,
@@ -24,12 +51,54 @@ export class Scanner {
                             current,
                         );
                         break;
+                    case "@": {
+                        const start = this.index - 1;
+                        while (
+                            !this.cannotAdvance() &&
+                            this.isWordCharacter(this.peek())
+                        ) {
+                            this.advance();
+                        }
+
+                        const word = this.source.substring(
+                            start,
+                            this.index,
+                        );
+                        yield this.createToken(
+                            TokenType.IDENTIFIER,
+                            word,
+                        );
+                        break;
+                    }
                     default:
-                        if (this.isAlphaNumeric(current)) {
+                        if (this.isNumber(current)) {
                             const start = this.index - 1;
                             while (
                                 !this.cannotAdvance() &&
-                                this.isAlphaNumeric(this.peek())
+                                (
+                                    this.isWordCharacter(this.peek()) ||
+                                    this.isNumberCharacter(this.peek())
+                                )
+                            ) {
+                                this.advance();
+                            }
+
+                            const amount = this.source.substring(
+                                start,
+                                this.index,
+                            );
+
+                            yield this.createToken(
+                                TokenType.AMOUNT,
+                                amount,
+                            );
+                        }
+
+                        if (this.isWordCharacter(current)) {
+                            const start = this.index - 1;
+                            while (
+                                !this.cannotAdvance() &&
+                                this.isWordCharacter(this.peek())
                             ) {
                                 this.advance();
                             }
@@ -53,8 +122,16 @@ export class Scanner {
         }
     }
 
-    private isAlphaNumeric(input: string) {
-        return input.match(/[A-Za-z\-_]/g)?.length == input.length;
+    private isWordCharacter(input: string) {
+        return input.match(/[A-Za-z\-_]/) !== null;
+    }
+
+    private isNumber(input: string) {
+        return input.match(/[0-9]/) !== null;
+    }
+
+    private isNumberCharacter(input: string) {
+        return input.match(/[0-9\.\/]/) !== null;
     }
 
     private createToken(tokenType: TokenType, literal: string): Token {
@@ -85,6 +162,7 @@ export const TokenType = {
     LEFT_CURLY_BRACE: 1,
     RIGHT_CURLY_BRACE: 2,
     DASH: 5,
+    COMMA: 8,
 
     NEWLINE: 4,
     WHITESPACE: 7,
--- tests/scanner.tests.ts
+++ tests/scanner.tests.ts
@@ -23,7 +23,7 @@ Deno.test("Can parse two words", () => {
 });
 
 Deno.test("Can parse braces", () => {
-    const input = "pbj { jelly peanut-butter }"
+    const input = "pbj { jelly peanut-butter }";
     const output = scanner.scan(input).toArray();
     assertEquals(
         output,
@@ -33,6 +33,89 @@ Deno.test("Can parse braces", () => {
             new Token(TokenType.WORD, "jelly"),
             new Token(TokenType.WORD, "peanut-butter"),
             new Token(TokenType.RIGHT_CURLY_BRACE, "}"),
-        ]
-    )
+        ],
+    );
 });
+
+Deno.test("Can parse newlines", () => {
+    const input = `
+pbj { 
+  jelly
+  peanut-butter
+}`.trim();
+    const output = scanner.scan(input).toArray();
+    assertEquals(
+        output,
+        [
+            new Token(TokenType.WORD, "pbj"),
+            new Token(TokenType.LEFT_CURLY_BRACE, "{"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.WORD, "jelly"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.WORD, "peanut-butter"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.RIGHT_CURLY_BRACE, "}"),
+        ],
+    );
+});
+
+Deno.test("Can parse - character", () => {
+    const input = `
+dip {
+  pita
+  hummus
+  - arrange on plate
+}`.trim();
+    const output = scanner.scan(input).toArray();
+    assertEquals(
+        output,
+        [
+            new Token(TokenType.WORD, "dip"),
+            new Token(TokenType.LEFT_CURLY_BRACE, "{"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.WORD, "pita"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.WORD, "hummus"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.DASH, "-"),
+            new Token(TokenType.WORD, "arrange"),
+            new Token(TokenType.WORD, "on"),
+            new Token(TokenType.WORD, "plate"),
+            new Token(TokenType.NEWLINE, "\n"),
+            new Token(TokenType.RIGHT_CURLY_BRACE, "}"),
+        ],
+    );
+});
+
+Deno.test("Can handle windows newlines", () => {
+    const input = "hello\r\nworld";
+    const output = scanner.scan(input).toArray();
+    assertEquals(output, [
+        new Token(TokenType.WORD, "hello"),
+        new Token(TokenType.NEWLINE, "\r\n"),
+        new Token(TokenType.WORD, "world"),
+    ]);
+});
+
+Deno.test("Can parse identifier", () => {
+    const input = "@foo";
+    const output = scanner.scan(input).toArray();
+    assertEquals(output, [new Token(TokenType.IDENTIFIER, input)]);
+});
+
+Deno.test("Can parse amount", () => {
+    const input = "1/2c"
+    const output = scanner.scan(input).toArray();
+    assertEquals(output, [new Token(TokenType.AMOUNT, "1/2c")])
+})
+
+Deno.test("Can parse comma", () => {
+    const input = "0.5tsp cumin, ground"
+    const output = scanner.scan(input).toArray();
+    assertEquals(output, [
+        new Token(TokenType.AMOUNT, "0.5tsp"),
+        new Token(TokenType.WORD, "cumin"),
+        new Token(TokenType.COMMA, ","),
+        new Token(TokenType.WORD, "ground")
+    ])
+})
