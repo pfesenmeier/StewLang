09815705600a35edf64d8cff2bb8bf14e0c2d4bf
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-07-23 01:55:37 +0000

    splash screen!

--- cli/components/Welcome.tsx
+++ cli/components/Welcome.tsx
@@ -1,10 +1,22 @@
 import Gradient from "ink-gradient";
+import process from "node:process";
 import BigText from "ink-big-text";
+import { Box, Text } from "ink";
 
 export default function Welcome() {
   return (
-    <Gradient name="rainbow">
-      <BigText text="StewLang" />
-    </Gradient>
+    <Box
+      flexDirection="column"
+      height={process.stdout.rows}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box>
+        <Gradient name="morning">
+          <BigText text="StewLang" />
+        </Gradient>
+      </Box>
+      <Text>[ Press any key to continue ]</Text>
+    </Box>
   );
 }
